5511c7338b6243eca2517f239df2816c5b81309d
viraladmin <00purple@gmail.com>
2019-07-17 01:42:04 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,40 +1,29 @@
 <?php
-session_start();
-//set timezone
-date_default_timezone_set('Europe/London');
-//database credentials
-define('DBHOST','localhost');
-define('DBUSER','YOUR_DB_USER');
-define('DBPASS','YOUR_DB_PASS');
-define('DBNAME','YOUR_DB_NAME');
+    session_start();
+    //set timezone
+    date_default_timezone_set('Europe/London');
 
+    //database credentials
+    define('DBHOST','localhost');
+    define('DBUSER','YOUR_DB_USER');
+    define('DBPASS','YOUR_DB_PASS');
+    define('DBNAME','YOUR_DB_NAME');
 
-try {
-
-        //create PDO connection
+    try {
         $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-} catch(PDOException $e) {
-        //show error
-    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
-    exit;
-}
-
-function getRealIpAddr()
-{
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-    {
-      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    } catch(PDOException $e) {
+        echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+        exit;
     }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-    {
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+
+    function getRealIpAddr() {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
-    else
-    {
-      $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
-}
