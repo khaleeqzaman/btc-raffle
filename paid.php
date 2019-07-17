c16b7f9985ea2b9177ab43f62bbe7104594597b3
viraladmin <00purple@gmail.com>
2019-07-17 01:45:02 -0600

    Update paid.php

--- paid.php
+++ paid.php
@@ -1,21 +1,20 @@
 <?php
-            echo '
-                <link rel="stylesheet" type="text/css" href="payment_modal.css">
-                <div id="modal1" class="overlay">
-                    <div class="modal" id="modal">
-                        <a class="cancel" href="">X</a>
-                        Update Patment Satus<br><br>
-                        <div class="content">
-                            <form method="post">
-                                <input type="hidden" name="id" value="'.$_GET['id'].'">
-                                <input style="width:70%;" type="text" name="pay" placeholder="Enter TXID of Payment">
-                                <button type="submit">Submit</button>
-                            </form>
-                        </div>
-                    </div>
+    echo '
+        <link rel="stylesheet" type="text/css" href="payment_modal.css">
+        <div id="modal1" class="overlay">
+            <div class="modal" id="modal">
+                <a class="cancel" href="">X</a>
+                Update Patment Satus<br><br>
+                <div class="content">
+                    <form method="post">
+                        <input type="hidden" name="id" value="'.$_GET['id'].'">
+                        <input style="width:70%;" type="text" name="pay" placeholder="Enter TXID of Payment">
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
-                <script>
-                    window.location.hash = "modal1";
-                </script>
-            ';
-?>
+            </div>
+        </div>
+        <script>
+            window.location.hash = "modal1";
+        </script>
+    ';
